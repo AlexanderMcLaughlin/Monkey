0e3ea3d7a5aadd388df21e6451f124daa3e35d10
Alexander McLaughlin <alex.mclaughlin@knights.ucf.edu>
2019-02-21 18:42:13 -0500

    General comment updates

--- src/monkey/monkey.java
+++ src/monkey/monkey.java
@@ -2,43 +2,45 @@ package monkey;
 
 import java.util.Scanner;
 
-public class monkey 
-{
+public class monkey {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+        //Read from stdin
         Scanner in = new Scanner(System.in);
-        
+        //Cycle through all the monkey tree layouts
         int n = in.nextInt();
+        //Get the next line character so there are no stdin problems
         in.nextLine();
         
-        for(int i=1; i<=n; i++)
-        {
+        for(int i=1; i<=n; i++) {
             String s = in.nextLine();
-            
+            //Simple recursive solution to find whether the left or right subtree has the 
+            //most monkeys on vines and then multiply by 2 for the height of the tree from
+            //the bottom up, because there can be only 2 possible subvines from each vine
+            //and each subvine must be equal to its counterpart 
             System.out.println(i+" "+solve(s));
         }
         
     }
     
-    static int solve(String s)
-    {
+    static int solve(String s) {
+        //Base cases if there are no monkeys on vines, or if there are only two monkeys on two vines
         if(s.equals("")) return 1;
         if(s.equals("[]")) return 2;
         
+        //Implement recursion otherwise
         return solve(s, 0, s.length()-1);
     }
     
-    static int solve(String s, int sI, int eI) 
-    {   
+    static int solve(String s, int sI, int eI) {   
+        //General base cases
         if((sI+1)==eI) return 2;
         if(sI>=eI) return 0;
         
         int i=sI;
         int sum=0;
         
-        do
-        {            
+        do {            
             i++;
             
             if(s.charAt(i)=='[') sum++;
@@ -46,11 +48,16 @@ public class monkey
             
         }while(sum!=0);
         
+        //Basically, whichever subtree has the higher number of branches, left or right, 
+        //will be what the other needs to be equal to for the monkeys to balance their weight
+        //therefore, since there can only be 2 possible sub branches and the left must equal the
+        //right, we can just say whichever is bigger multiplied by 2 is the number of monkeys needed
+        //this is done from the top down to determine which subtree has more monkeys
         return 2 * max(solve(s, sI+1, i), solve(s, i+1, eI-1));
     }
     
-    static int max(int a, int b)
-    {
+    //Simple max function for two integers to avoid import
+    static int max(int a, int b) {
         if(a>b) return a;
         return b;
     }
